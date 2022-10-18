a4d5fd28ce5f93a03ed1c09f010eda6e5d3f208d
Denis Rysavy <rysavydenis3@gmail.com>
2022-10-18 18:52:36 +0200

    more Components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import Header from './Header';
+import Input from '../Components/Input';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Input />
     </div>
   );
 }
---
+++ src/Components/Input.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Input = () => {
+  return (
+    <div>
+        <h1>Input</h1>
+        <input id="neuTodo" 
+               name="neuToDo" 
+               type="text" 
+               placeholder="Aufgabe hinzufügen">
+        </input>
+
+    </div>
+  )
+}
+
+export default Input
